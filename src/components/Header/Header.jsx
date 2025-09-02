e496081d3377f5d2195276f57d6d2699e97bfe3f
Brian Ressler <carlofabyss@hotmail.com>
2025-09-01 21:52:00 -0500

    Add a basic header functional component to ensure the integration is working

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div className="eccent_header">
+            <ul>
+                <li key="nav-one">Home</li>
+                <li key="nav-two">About</li>
+                <li key="nav-three">Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header;
