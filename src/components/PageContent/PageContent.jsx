4003ff6c2233d9218c73c0c31b23c85577bf7c98
Brian Ressler <carlofabyss@hotmail.com>
2025-09-06 15:26:06 -0500

    Fix issue with overflow not getting rounded by adding another container.

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -1,8 +1,10 @@
 function PageContent({children}) {
     return (
-        <div id="eccent-page-content" className="absolute top-0 left-0 w-full h-full z-2 py-2 pr-2 pl-[70px] md:pl-[300px]">
-            <div id="eccent-page-content-inner" className="flex flex-col bg-content-bg h-full w-full rounded-md">
-                {children}
+        <div id="eccent-page-content" className="absolute top-0 left-0 w-full h-full z-2 py-2 pr-2 pl-[70px] md:pl-[300px] overflow-hidden">
+            <div id="eccent-page-content-inner" className="flex flex-col bg-content-bg h-full w-full rounded-md overflow-hidden">
+                <div id="eccent-page-content-scroll" className="flex flex-col w-full h-full overflow-y-auto overflow-x-hidden">
+                    {children}
+                </div>
             </div>
         </div>
     );
