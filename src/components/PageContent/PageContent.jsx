5509dfee79f86aac0ec05f6e82921436eff08180
Brian Ressler <carlofabyss@hotmail.com>
2025-09-06 15:15:32 -0500

    Add some basic globals for headings and paragraph text in terms of sizing and color.

--- src/components/PageContent/PageContent.jsx
+++ src/components/PageContent/PageContent.jsx
@@ -1,7 +1,7 @@
 function PageContent({children}) {
     return (
         <div id="eccent-page-content" className="absolute top-0 left-0 w-full h-full z-2 py-2 pr-2 pl-[70px] md:pl-[300px]">
-            <div id="eccent-page-content-inner" className="flex flex-col bg-stone-200 h-full w-full">
+            <div id="eccent-page-content-inner" className="flex flex-col bg-content-bg h-full w-full rounded-md">
                 {children}
             </div>
         </div>
